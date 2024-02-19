9cd98ffb3bc54df6ea2b9f216da3b5b56882daa5
GwynethS <gwynethsegura@gmail.com>
2024-02-19 11:53:52 -0500

    feat: se enlazó el carrito de compras a la página de contáctanos

--- js/contactanos.js
+++ js/contactanos.js
@@ -1,3 +1,20 @@
+import { ShoppingCart } from "./models/shoppingCart.js";
 import { activeState } from "./functions/activeState.js";
 
+// SHOPPING CART ELEMENTS
+const shoppingCartIconNav = document.getElementById("shopping-cart-icon-nav");
+const btnCloseCart = document.getElementById("btn-close-cart");
+
+const shoppingCart = new ShoppingCart();
+
 activeState();
+
+// SHOPPING CART
+shoppingCartIconNav.addEventListener("click", () => {
+  shoppingCart.showCartProducts();
+  shoppingCart.showCartTotalPayment();
+});
+
+btnCloseCart.addEventListener("click", () => {
+  shoppingCart.closeCart();
+});
